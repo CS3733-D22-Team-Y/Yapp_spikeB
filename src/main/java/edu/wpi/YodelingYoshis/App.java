f20c798b7b1815a6d6e0cbb36e884dcd9368a4b2
Nathan Anderson <nanderson2@wpi.edu>
2022-03-23 23:09:56 -0400

    Request Template Controller

--- src/main/java/edu/wpi/YodelingYoshis/App.java
+++ src/main/java/edu/wpi/YodelingYoshis/App.java
@@ -1,6 +1,11 @@
 package edu.wpi.YodelingYoshis;
 
+import java.io.IOException;
+import java.util.Objects;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,7 +18,18 @@ public class App extends Application {
   }
 
   @Override
-  public void start(Stage primaryStage) {}
+  public void start(Stage primaryStage) {
+    FXMLLoader loader = new FXMLLoader();
+    try {
+      Parent root =
+          loader.load(
+              Objects.requireNonNull(getClass().getResource("/views/request_template.fxml")));
+      primaryStage.setScene(new Scene(root));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    primaryStage.show();
+  }
 
   @Override
   public void stop() {
